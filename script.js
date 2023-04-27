fb260d29f7cf9cd036c3db7a89c66a9ba7893b95
Julia-Cherkasova <juliadzhepa@gmail.com>
2023-04-27 19:43:20 -0400

    Add function playRound

--- script.js
+++ script.js
@@ -4,7 +4,30 @@ let choice = [
     'Scissors'
 ];
 
-function computerChoice(){
+function getComputerChoice(){
 return choice[Math.floor(Math.random()*choice.length)];
 }
 
+function playRound (computerChoice, playerChoice){
+    if (computerChoice == "Rock" && playerChoice == "Scissors"){
+        return "You Lose! Rock beats scissors!";
+    }
+    else if(computerChoice == "Scissors" && playerChoice == "Rock"){
+        return "You win!!! Rock beats scissors!";
+    }
+    else if (computerChoice == "Scissors" && playerChoice == "Paper"){
+        return "You Lose! Scissors beats Paper!";
+    }
+    else if (computerChoice == "Paper" && playerChoice == "Scissors"){
+        return "You win!!!  Scissors beats Paper!";
+    }
+    else if (computerChoice == "Paper" && playerChoice == "Rock"){
+        return "You Lose! Paper beats rock!";
+    }
+    else if (computerChoice == "Rock" && playerChoice == "Paper"){
+        return "You win!!! Paper beats rock!";
+    }
+    else {
+        return "Something went wrong";
+    }
+}
