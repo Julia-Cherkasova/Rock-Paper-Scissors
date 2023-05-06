e4912f25fba32d0fa41dd67ef28043f6a7152a1d
Julia-Cherkasova <juliadzhepa@gmail.com>
2023-05-05 21:55:26 -0400

    Changed all variables in JS file. Add HTML file.

--- script.js
+++ script.js
@@ -1,33 +1,41 @@
-let choice = [
-    'Rock',
-    'Paper',
-    'Scissors'
-];
+let choice = ["rock", "paper", "scissors"];
+let playerSelection = document.querySelector(".player");
+let computerCount = "";
+let playerCount = "";
+let out = document.querySelector(".out-1");
 
 function getComputerChoice(){
-return choice[Math.floor(Math.random()*choice.length)];
-}
+    return choice[Math.floor(Math.random()*choice.length)];
+    }
 
-function playRound (computerChoice, playerChoice){
-    if (computerChoice == "Rock" && playerChoice == "Scissors"){
-        return "You Lose! Rock beats scissors!";
+let computerChoice = getComputerChoice()
+
+function playRound (){
+
+    let playerChoiceValue = playerSelection.value;
+
+    if (computerChoice == "rock" && playerChoiceValue.toLowerCase() == "scissors"){
+        out.innerHTML = "You Lose! Rock beats scissors!";
     }
-    else if(computerChoice == "Scissors" && playerChoice == "Rock"){
-        return "You win!!! Rock beats scissors!";
+    else if(computerChoice == "scissors" && playerChoiceValue.toLowerCase() == "rock"){
+        out.innerHTML = "You win!!! Rock beats scissors!";
     }
-    else if (computerChoice == "Scissors" && playerChoice == "Paper"){
-        return "You Lose! Scissors beats Paper!";
+    else if (computerChoice == "scissors" && playerChoiceValue.toLowerCase() == "paper"){
+        out.innerHTML = "You Lose! Scissors beats Paper!";
     }
-    else if (computerChoice == "Paper" && playerChoice == "Scissors"){
-        return "You win!!!  Scissors beats Paper!";
+    else if (computerChoice == "paper" && playerChoiceValue.toLowerCase() == "scissors"){
+        out.innerHTML = "You win!!! Scissors beats Paper!";
     }
-    else if (computerChoice == "Paper" && playerChoice == "Rock"){
-        return "You Lose! Paper beats rock!";
+    else if (computerChoice == "paper" && playerChoiceValue.toLowerCase() == "rock"){
+        out.innerHTML = "You Lose! Paper beats rock!";
     }
-    else if (computerChoice == "Rock" && playerChoice == "Paper"){
-        return "You win!!! Paper beats rock!";
+    else if (computerChoice == "rock" && playerChoiceValue.toLowerCase() == "paper"){
+        out.innerHTML = "You win!!! Paper beats rock!";
     }
     else {
-        return "Something went wrong";
+        out.innerHTML = "Something went wrong";
     }
 }
+
+document.querySelector(".b-1").onclick = playRound;
+
